19c9d9a88d3ededd059cdc27f62a409ab27298b1
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-04-29 01:23:24 +0200

    Created Login Activity

--- app/src/main/java/com/motivateme/LoginActivity.java
+++ app/src/main/java/com/motivateme/LoginActivity.java
@@ -1,16 +1,15 @@
 package com.motivateme;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        startActivity(new Intent(this, LoginActivity.class));
     }
+
 }
