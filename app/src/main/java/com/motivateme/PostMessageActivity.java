acbaf9852bb065aa304a3833b292fb2f7e08d4f6
Kappa013 <ferhat.yu@hotmail.com>
2016-06-09 10:54:16 +0200

    Start on post message screen and activity

--- app/src/main/java/com/motivateme/PostMessageActivity.java
+++ app/src/main/java/com/motivateme/PostMessageActivity.java
@@ -2,7 +2,6 @@ package com.motivateme;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,30 +9,18 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class PostMessageActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_postmessage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_postmessage);
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //.setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -44,23 +31,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ListView mFeed = (ListView) findViewById(R.id.lvDailyFeed);
-        final FeedItem[] items = {
-                new FeedItem("\"I'm very rich\"", R.drawable.donald_trump_workout, "Golfing with Barrack", new Goal("My first hole in one", 800, 0, 1), new Profile("TheDonald", "Donald Trump", 69, R.drawable.donald_trump)),
-                new FeedItem("To succeed, you have to believe everynight in your heart, that you\'re the best", R.drawable.john_cena_workout, "Bench pressing", new Goal("Bench press 500", 800, 485, 500), new Profile("HisNameJohnCena", "John Cena", 38, R.drawable.john_cena)),
-                new FeedItem("Don't think about the start of the race. Think about the ending.", R.drawable.usain_bolt_workout, "Practicing in Kingston", new Goal("100 metres in less than 9.58 seconds", 1000, 9, 10), new Profile("Bolt", "Usain Bolt", 29, R.drawable.usain_bolt)),
-                new FeedItem("Failure is not an option for me. Success is all I envision", R.drawable.mcgregor_workout, "Training to knock Nate Diaz out", new Goal("Do 150 push ups in a row", 700, 102, 150), new Profile("Notorious", "Conor McGregor", 27, R.drawable.mcgregor))
-        };
-
-        mFeed.setAdapter(new FeedAdapter(this, items));
-        mFeed.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(MainActivity.this, ViewFeedActivity.class)
-                        .putExtra("feedItem", items[position]));
-            }
-        });
     }
+
 
     @Override
     public void onBackPressed() {
