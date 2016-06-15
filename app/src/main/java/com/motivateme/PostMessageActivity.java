aae4006783f6277872ee81f18769d1a599b4b0bd
Kappa013 <ferhat.yu@hotmail.com>
2016-06-15 12:29:02 +0200

    Deleted unused menu code from PostActivity

--- app/src/main/java/com/motivateme/PostMessageActivity.java
+++ app/src/main/java/com/motivateme/PostMessageActivity.java
@@ -1,17 +1,12 @@
 package com.motivateme;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class PostMessageActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class PostMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +30,5 @@ public class PostMessageActivity extends AppCompatActivity
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        Intent intent;
-
-        switch (id) {
-            case R.id.nav_challenge_yourself:
-                startActivity(new Intent(this, ChallengeYourselfActivity.class));
-                break;
-            case R.id.nav_goals:
-                //intent = new Intent(this, )
-                break;
-            case R.id.nav_tag_friend:
-                //intent = new Intent(this, )
-                break;
-            case R.id.nav_friends:
-                //intent = new Intent(this, )
-                break;
-            case R.id.nav_rewards:
-                //intent = new Intent(this, )
-                break;
-            case R.id.nav_settings:
-                //intent = new Intent(this, )
-                break;
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
