f3a1459f22ebdc4bf8be7700e4b011bbdbc361d2
Kappa013 <ferhat.yu@hotmail.com>
2016-06-15 12:44:57 +0200

    Add pictures to Post

--- app/src/main/java/com/motivateme/PostMessageActivity.java
+++ app/src/main/java/com/motivateme/PostMessageActivity.java
@@ -1,13 +1,22 @@
 package com.motivateme;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class PostMessageActivity extends AppCompatActivity {
+
+    private final static int PICK_PHOTO_CODE = 0;
+    private final static int CAMERA_REQUESTED = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,24 @@ public class PostMessageActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        BitmapDrawable selectedImage = null;
+        if (requestCode == CAMERA_REQUESTED && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            selectedImage = new BitmapDrawable(getResources(), (Bitmap) extras.get("data"));
+        } else {
+            try {
+                selectedImage = new BitmapDrawable(getResources(), MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ImageView mPickPhoto = (ImageView) findViewById(R.id.imgViewPost);
+        assert mPickPhoto != null;
+        mPickPhoto.setBackground(selectedImage);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_post, menu);
         return true;
@@ -32,7 +59,13 @@ public class PostMessageActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_picture:
-                Toast.makeText(this, "Implement feed searching", Toast.LENGTH_SHORT).show();
+                //make the user pick a specific photo
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    //bring up gallery to select a photo
+                    startActivityForResult(intent, PICK_PHOTO_CODE);
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
